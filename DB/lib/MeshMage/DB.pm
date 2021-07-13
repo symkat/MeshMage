0dae67e50d2a53ad15abf1ae0b4a2d17a38211c3
Kaitlyn Parkhurst <symkat@symkat.com>
2021-07-12 21:40:27 -0700

    Add DeploymentHandler stuff.

--- DB/lib/MeshMage/DB.pm
+++ DB/lib/MeshMage/DB.pm
@@ -17,6 +17,11 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-05-31 02:44:22
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:hfpUgdlv0nA10HU5quS9+A
 
+our $VERSION = 1;
+
+sub install_defaults {
+
+}
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
